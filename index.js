616303a6984c30cc1729c2f84dff34d1a0fdc8ec
Ninian Bond <ninrhythm@gmail.com>
2025-11-12 09:02:52 -0500

    added solution and README.md

---
+++ index.js
@@ -0,0 +1,15 @@
+function getGrade (s1, s2, s3) {
+  let sum = s1 + s2 + s3
+  let avg = sum / 3
+  if (avg <= 100 && avg >= 90) {
+    return "A"
+  } else if (avg < 90 && avg >= 80) {
+    return "B"
+  } else if (avg < 80 && avg >= 70) {
+    return "C"
+  } else if (avg < 70 && avg >= 60) {
+    return "D"
+  } else if (avg < 60 && avg >= 0) {
+    return "F"
+  }
+}
